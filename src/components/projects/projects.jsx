27e729c7ee02c708f7871e6c0ac8ce9d0d03c9ff
SavindaKaveesha <115203716+SavindaKaveesha@users.noreply.github.com>
2024-01-09 20:01:24 +0530

    projects edited

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './projects.css'
-import SlidingCardImg from '../../assets/number gussig game.png'
+import NumberGussingGameIMG from '../../assets/number gussig game.png'
+import StudentManagmentSystemIMG from '../../assets/Student Managment.png';
+import ShopManageIMG from '../../assets/Shop Manage.png';
+import BlackJackGameIMG from '../../assets/Black Jack Game.png' 
+import TodoAppIMG from '../../assets/Todo App.png';
+import MediaPlayerIMG from '../../assets/Media Player.png';
 import SlidingCard from './sliding card/SlidingCard';
 
 function Projects() {
@@ -10,34 +15,40 @@ function Projects() {
         <span className="projectDesc">vfdvfdvfdvbkfbk bfdk jfjhvjfhdjajvbjb jb jfb jifji bkfjadj fb jfbj bdfjh kjfjk fkj jkfb jkbfjb jfj bfjk jfbj bkjfdfbj dfk sdf hfl fb bvlb hlfdbl shfbh blsdfj blfj bfj bjdsl jb f kjfjknfjn jkfjka nfkjd jnf njk </span>
         <div className="projectImgs">
           <SlidingCard
-            title="Project 1"
-            description="This is the description for Project 1."
-            image={SlidingCardImg}
+            title="Student Managment System"
+            description="C# | .Net | Firebase"
+            image={StudentManagmentSystemIMG}
           />
 
           <SlidingCard
-            title="Project 1"
-            description="This is the description for Project 1."
-            image={SlidingCardImg}
+            title="Book Shop Managment System"
+            description="C# | .Net | MSSQL"
+            image={ShopManageIMG}
           />
 
           <SlidingCard
             title="Number Guessing Game"
             description="HTML | CSS | ReactJS"
-            image={SlidingCardImg}
+            image={NumberGussingGameIMG}
           />
 
           <SlidingCard
-            title="Project 1"
-            description="This is the description for Project 1."
-            image={SlidingCardImg}
+            title="Black Jack Game"
+            description="HTML | CSS | Javascript"
+            image={BlackJackGameIMG}
           />
 
           <SlidingCard
-            title="Project 1"
-            description="This is the description for Project 1."
-            image={SlidingCardImg}
+            title="Media Player"
+            description="C# | .Net"
+            image={MediaPlayerIMG}
           />
+
+          <SlidingCard
+            title="ToDo List "
+            description="Python | Flask | HTML | CSS | MongoDB"
+            image={TodoAppIMG}
+            />
 
         </div>
         <a href='https://github.com/SavindaKaveesha?tab=repositories'>
